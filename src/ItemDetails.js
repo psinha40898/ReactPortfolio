6e3219a2312194866bfea8fa629fd2a059ebfa2d
pyush <psinha5@horizon.csueastbay.edu>
2023-05-07 09:46:16 -0700

    5-7-2023 working prototype

---
+++ src/ItemDetails.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ItemDetails = ({ item }) => {
+  return <div>{item}</div>;
+};
+
+export default ItemDetails;
---
+++ src/transform.js
@@ -0,0 +1,106 @@
+import React, { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+
+
+const TextTransformer = ({ textList, middleList, resultsList }) => {
+    const [text, setText] = useState(textList[0]);
+    const [testBool, setBool] = useState(false);
+    const [incrementation, setIncrement] = useState(-1);
+
+
+  
+    const changeText = () => {
+      const currentIndex = textList.indexOf(text);
+
+      const newIncrementation = incrementation + 1;
+      console.log("transform.js debugger: The index" + currentIndex);
+      console.log("transform.js debugger: The top result is " + resultsList[0].props.children);
+
+
+      // if (currentIndex === 1)
+      // {
+      //   setText(textList[0])
+      //   setBool(false);
+      // }
+      // else
+      // {
+      //   setText(textList[1])
+      //   setBool(true);
+      // }
+      console.log(testBool);
+      if (newIncrementation === 0)
+      {
+        setText(textList[0]);
+        setIncrement(incrementation+1);
+      }
+      else if (newIncrementation === 1){
+        setText(middleList[0]);
+        setIncrement(incrementation+1);
+
+
+      }
+      else if (newIncrementation === 2) {
+        setText(resultsList[0]);
+        setIncrement(incrementation+1);
+        
+      }
+      else
+      {
+        setText("");
+        setIncrement(-1);
+        return;
+      }
+   
+    //   const nextIndex = (currentIndex + 1) % textList.length;
+    //   setText(textList[nextIndex]);
+    };
+
+    const renderElements = (index) => {
+      return (
+        <AnimatePresence key={index}>
+          {text === textList[0] && (
+            <motion.div
+              key="A"
+              initial={{ opacity: 0, scale: 0.75 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ type: 'tween' }}
+            >
+              {textList[index]}
+            </motion.div>
+          )}
+          {text === middleList[0] && (
+            <motion.div
+              key="B"
+              initial={{ opacity: 0, scale: 0.75 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ type: 'tween', delay: 0.75*(1/(index+.65))}}
+            >
+              {middleList[index]}
+            </motion.div>
+          )}
+          {text === resultsList[0] && (
+            <motion.div
+              key="C"
+              initial={{ opacity: 0, scale: 0.75 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ type: 'tween', delay: 0.75*(1/(index+.65))}}
+            >
+              {resultsList[index]}
+            </motion.div>
+          )}
+        </AnimatePresence>
+      );
+    };
+  
+    return (
+      <div>
+        {textList.map((_, index) => renderElements(index))}
+        <button onClick={changeText}>Toggle Text</button>
+      </div>
+    );
+  };
+  
+  export default TextTransformer;
